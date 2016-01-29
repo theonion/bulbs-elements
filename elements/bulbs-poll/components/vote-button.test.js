fe305f70c443f22fcb3fb214c2e03da251fa4bd2
Collin Miller <collintmiller@gmail.com>
2016-01-28 18:20:08 -0600

    stub test for vote-button

---
+++ elements/bulbs-poll/components/vote-button.test.js
@@ -0,0 +1,35 @@
+import VoteButton from './vote-button';
+import React from 'react';
+import { assertJSXEqual } from 'bulbs-elements/test/assertions';
+
+describe('VoteButton', function () {
+  context('with selectedAnswer', function () {
+    it('renders active vote button', function () {
+      let props = {
+        selectedAnswer: { id: 1 },
+        makeVoteRequest: function () {},
+      };
+
+      assertJSXEqual(<VoteButton {...props} />,
+        <button className="bulbs-poll-vote">
+          Vote
+        </button>
+      );
+    });
+  });
+
+  context('without selectedAnswer', function () {
+    it('renders inactive vote button', function () {
+      let props = {
+        selectedAnswer: null,
+        makeVoteRequest: function () {},
+      };
+
+      assertJSXEqual(<VoteButton {...props} />,
+        <button className="bulbs-poll-vote">
+          Vote
+        </button>
+      );
+    });
+  });
+});
