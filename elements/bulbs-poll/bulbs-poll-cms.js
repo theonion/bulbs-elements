6cfa0adec4717f0333257a4a4f1ddc8436371a51
Collin Miller <collintmiller@gmail.com>
2016-03-02 16:53:04 -0600

    inlne titles in embedded polls

--- elements/bulbs-poll/bulbs-poll-cms.js
+++ elements/bulbs-poll/bulbs-poll-cms.js
@@ -1,11 +1,33 @@
 import { registerForBulbsCMS, EmbededCMSElement } from 'bulbs-elements/register';
 
+let promiseCache = {}
+let previewDataCache = {};
+function getPreviewData (src, callback) {
+  promiseCache[src] || (promiseCache[src] = fetch(src).then((response) => response.json()));
+  promiseCache[src].then((data) => {
+    previewDataCache[src] = data;
+    callback(data);
+  });
+}
+
 class EmbeddedBulbsPoll extends EmbededCMSElement {
   get embedContentPreview () {
+    let src = this.getAttribute('src');
+    let title = '';
+    if (src) {
+      if (previewDataCache[src]) {
+        title = previewDataCache[src].title;
+      }
+      else {
+        getPreviewData(src, (data) => {
+          this.querySelector('h1').innerHTML += ` ${data.title}`;
+        });
+      }
+    }
     return `
       <h1 style='text-align: center; font-family: "Droid Serif"'>
         <i class='fa fa-th-list'></i>
-        Embedded Poll
+        Embedded Poll: ${title}
       </h1>
     `;
   }
