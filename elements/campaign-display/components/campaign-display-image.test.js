6394a3cbe0a2706e7143302ac9d75bd9622a0e0c
Dayton Nolan <daytonn@gmail.com>
2016-04-04 17:19:13 -0500

    Add tests for campaign display component

--- elements/campaign-display/components/campaign-display-image.test.js
+++ elements/campaign-display/components/campaign-display-image.test.js
@@ -2,18 +2,18 @@ import React from 'react';
 import CampaignDisplayImage from './campaign-display-image';
 import { createRenderer } from 'react-addons-test-utils';
 
-describe('<campaign-display> <CampaignDisplayImage>', function () {
+describe('<campaign-display> <CampaignDisplayImage>', () => {
   let shallowRenderer;
   let props;
   let subject;
-  beforeEach(function() {
+  beforeEach(() => {
     props = { image: 'some-image-url' };
     shallowRenderer = createRenderer();
     shallowRenderer.render(<CampaignDisplayImage {...props} />);
     subject = shallowRenderer.getRenderOutput();
   });
 
-  it('renders the campaign image', function () {
+  it('renders the campaign image', () => {
     expect(subject.props.children.type).to.equal('img');
     expect(subject.props.children.props.src).to.equal('some-image-url');
   });
--- elements/campaign-display/components/campaign-display-name.test.js
+++ elements/campaign-display/components/campaign-display-name.test.js
@@ -2,18 +2,18 @@ import React from 'react';
 import CampaignDisplayName from './campaign-display-name';
 import { createRenderer } from 'react-addons-test-utils';
 
-describe('<campaign-display> <CampaignDisplayName>', function () {
+describe('<campaign-display> <CampaignDisplayName>', () => {
   let shallowRenderer;
   let props;
   let subject;
-  beforeEach(function() {
+  beforeEach(() => {
     props = { name: 'Test Campaign' };
     shallowRenderer = createRenderer();
     shallowRenderer.render(<CampaignDisplayName {...props} />);
     subject = shallowRenderer.getRenderOutput();
   });
 
-  it('renders the campaign name', function () {
+  it('renders the campaign name', () => {
     expect(subject.props.children).to.contain('Test Campaign');
   });
 });
