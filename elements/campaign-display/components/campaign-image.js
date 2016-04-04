ad422fee9525b70891af2186c5401e2030ca7fb1
Andrew Kos <akos123@gmail.com>
2016-04-04 10:46:24 -0500

    added image and name components

---
+++ elements/campaign-display/components/campaign-image.js
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+import classnames from 'classnames';
+
+export default function CampaignImage (props) {
+  let className = classnames('campaign-display-campaign-image', {});
+
+  return (
+    <div className={className}>
+    </div>
+  );
+}
+
+CampaignImage.propTypes = {
+
+};
