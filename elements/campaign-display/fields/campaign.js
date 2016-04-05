df1dce3083c074b52c6cb0cc3a52cc455d65ece3
Dayton Nolan <daytonn@gmail.com>
2016-04-05 16:31:08 -0500

    Remove jquery from tests

--- elements/campaign-display/fields/campaign.js
+++ elements/campaign-display/fields/campaign.js
@@ -1,38 +1,38 @@
-import { Field, Action } from 'bulbs-elements/store';
-
-const CampaignField = new Field({
+const CampaignField = {
   initialState: {
     data: [],
     requestInFlight: false,
   },
 
-  fetchCampaign: new Action(function (state, campaignUrl, store) {
-    state.requestInFlight = true;
-    this.request(campaignUrl, {
-      success: store.actions.fetchCampaignSuccess,
-      failure: store.actions.fetchCampaignFailure,
-      error: store.actions.fetchCampaignError,
-    });
-    return state;
-  }),
+  actions: {
+    fetchCampaign(state, campaignUrl, store) {
+      state.requestInFlight = true;
+      this.request(campaignUrl, {
+        success: store.actions.fetchCampaignSuccess,
+        failure: store.actions.fetchCampaignFailure,
+        error: store.actions.fetchCampaignError,
+      });
+      return state;
+    },
 
-  fetchCampaignSuccess: new Action(function (state, data) {
-    state.data = data;
-    state.requestInFlight = false;
-    return state;
-  }),
+    fetchCampaignSuccess(state, data) {
+      state.data = data;
+      state.requestInFlight = false;
+      return state;
+    },
 
-  fetchCampaignFailure: new Action(function (state, failure) {
-    state.requestFailure = failure;
-    state.requestInFlight = false;
-    return state;
-  }),
+    fetchCampaignFailure(state, failure) {
+      state.requestFailure = failure;
+      state.requestInFlight = false;
+      return state;
+    },
 
-  fetchCampaignError: new Action(function (state, error) {
-    state.requestError = error;
-    state.requestInFlight = false;
-    return state;
-  }),
-});
+    fetchCampaignError(state, error) {
+      state.requestError = error;
+      state.requestInFlight = false;
+      return state;
+    },
+  },
+};
 
 export default CampaignField;
