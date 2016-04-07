85171f155665e10b80cb551034ed3638f5d038b2
Dayton Nolan <daytonn@gmail.com>
2016-04-07 17:03:30 -0500

    Protect against missing urls and network errors

--- elements/campaign-display/components/campaign-display-empty.test.js
+++ elements/campaign-display/components/campaign-display-empty.test.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import _ from 'lodash';
+import _, { first } from 'lodash';
 import utils, { createRenderer } from 'react-addons-test-utils';
 import rquery from 'rquery';
-import CampaignDisplayName from './campaign-display-name';
+import CampaignDisplayEmpty from './campaign-display-empty';
 import fetchMock from 'fetch-mock';
 
 const $R = rquery(_, React, ReactDOM, utils);
 
-describe('<campaign-display> <CampaignDisplayName>', () => {
+describe('<campaign-display> <CampaignDisplayEmpty>', () => {
   let shallowRenderer;
-  let props;
   let subject;
   let campaignUrl;
   beforeEach(() => {
     campaignUrl = 'http://example.com';
-    fetchMock.mock(campaignUrl, 200);
-    props = { name: 'Test Campaign' };
+    fetchMock.mock(campaignUrl, 400);
     shallowRenderer = createRenderer();
-    shallowRenderer.render(<CampaignDisplayName {...props} />);
+    shallowRenderer.render(<CampaignDisplayEmpty />);
     subject = $R(shallowRenderer.getRenderOutput());
   });
 
-  it('renders the campaign name', () => {
-    expect(subject.text()).to.equal('Test Campaign');
+  it('renders nothing', () => {
+    let children = first(subject.components).props.children;
+    expect(children).to.be.undefined;
   });
 });
