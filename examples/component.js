d5edf041482f18979dc16075644b614652f2cc34
Collin Miller (Zardulist) <collintmiller@gmail.com>
2016-04-12 15:17:45 -0500

    added catch-all route and error handler for examples viewer

--- examples/component.js
+++ examples/component.js
@@ -2,19 +2,71 @@ import React from 'react';
 import examples from './data';
 
 export default class Component extends React.Component {
-  render () {
-    return (
-      <div dangerouslySetInnerHTML={this.renderExample()} />
-    );
-  }
-
   renderExample () {
-    let example = examples.find((example) => {
-      return example.element === this.props.params.element;
+    let example = examples.find((anExample) => {
+      return anExample.element === this.props.params.element;
     });
     return {
       __html: example.examples[this.props.params.example].render(),
     };
+  }
+
+  render () {
+    try {
+      return (
+        <div dangerouslySetInnerHTML={this.renderExample()} />
+      );
+    }
+    catch (error) {
+      let knownError;
+      switch (error.message) {
+        case "Cannot read property 'render' of undefined":
+          knownError = "The example for this url does not exist."
+          break;
+      }
+      return (
+        <div
+          style={{
+            padding: '1em',
+          }}
+        >
+          <code>
+            <h1
+              style={{
+                color: 'red',
+              }}
+            >
+              Failed to render example at:
+            </h1>
+            <a href={location.href}>
+              { location.href }
+            </a>
+
+            <h2
+              style={{
+              }}
+            >
+              {knownError}  
+            </h2>
+
+            <hr/>
+            {
+              error.stack.split('\n').map((line) => {
+                return (
+                  <div
+                    style={{
+                      whiteSpace: 'nowrap',
+                    }}
+                  >
+                    {line}
+                  </div>
+                );
+              })
+            }
+          </code>
+        </div>
+      );
+    }
   }
 }
 
--- examples/routes.js
+++ examples/routes.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Router, Route } from 'react-router';
-import createBrowserHistory from 'history/lib/createBrowserHistory'
+import {
+  Router,
+  Route,
+} from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 let browserHistory = createBrowserHistory();
 
 import Index from './index';
 import Component from './component';
 
+function Empty (props) {
+  return <h1> Choose an Example </h1>;
+}
+
 const Routes = (
   <Router history={browserHistory}>
     <Route path="/" component={Index}>
       <Route path="example/:element/:example" component={Component} />
+      <Route path="*" component={Empty}>
+      </Route>
     </Route>
   </Router>
 );
 
-export default Routes
+export default Routes;
