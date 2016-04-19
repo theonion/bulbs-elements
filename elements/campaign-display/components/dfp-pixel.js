5a474e9d9fc14cf2814be7e9ae77d9b74b25156c
Andrew Kos <akos123@gmail.com>
2016-04-19 12:23:55 -0500

    callback via emitted event

--- elements/campaign-display/components/dfp-pixel.js
+++ elements/campaign-display/components/dfp-pixel.js
@@ -1,17 +1,27 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
-export default function DfpPixel (props) {
-  let targeting = {
-    dfp_placement: props.placement,
-    dfp_campaign_id: props.campaignId,
-  };
+export default class DfpPixel extends Component {
 
-  return (
-    <div
-        data-ad-unit="campaign-pixel"
-        data-targeting={ JSON.stringify(targeting) }>
-    </div>
-  );
+  componentDidMount () {
+    let onRender = document.createEvent('Event');
+    onRender.initEvent('campaign-display-dfp-pixel-ready', true, true);
+    this.refs.container.dispatchEvent(onRender);
+  }
+
+  render () {
+    let targeting = {
+      dfp_placement: this.props.placement,
+      dfp_campaign_id: this.props.campaignId,
+    };
+
+    return (
+      <div
+          ref="container"
+          data-ad-unit="campaign-pixel"
+          data-targeting={ JSON.stringify(targeting) }>
+      </div>
+    );
+  }
 }
 
 DfpPixel.propTypes = {
