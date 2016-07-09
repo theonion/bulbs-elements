68fe0472332471191ab1b762ccd202aaf50e1c74
Melissa <melpatenator@gmail.com>
2016-07-08 20:22:51 -0500

    add disqus to the mix ;-)

---
+++ elements/share-tools/components/via-disqus.js
@@ -0,0 +1,33 @@
+/* global BULBS_ELEMENTS_ANALYTICS_MANAGER */
+
+import React, { PropTypes } from 'react';
+import ShareTool from './share-tool';
+import ShareButton from './share-button';
+
+export default class CommentViaDisqus extends ShareTool {
+  constructor (props) {
+    super(props);
+    this.share = this.share.bind(this);
+  }
+
+  share (event) {
+    event.preventDefault();
+    window.open(
+      this.shareUrl + '#comments'
+    );
+  }
+
+  render () {
+    return (
+      <ShareButton
+        className='share-via-disqus'
+        dataTrackLabel='Disqus'
+        iconClassName='fa fa-comments'
+        icon={this.hasIcon()}
+        label={this.hasLabel()}
+        labelText='Disqus'
+        onClick={this.share}
+      />
+    );
+  }
+}
