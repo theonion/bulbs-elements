09c7be02cd8b8a013405f1015efa493a36772aa4
Collin Miller (Zardulist) <collintmiller@gmail.com>
2016-07-27 14:49:40 -0500

    minor updates to example runner

--- examples/component.js
+++ examples/component.js
@@ -12,9 +12,14 @@ class Component extends React.Component {
       return exmpl.element === this.props.params.element;
     });
   }
+
+  get exampleSpec () {
+    return this.example.examples[this.props.params.example];
+  }
+
   renderExample () {
     return {
-      __html: this.example.examples[this.props.params.example].render(),
+      __html: this.exampleSpec.render(),
     };
   }
 
@@ -23,16 +28,22 @@ class Component extends React.Component {
     this.testChangeSource.onmessage = () => {
       this.refs.iframe.src = this.refs.iframe.src;
     };
+    if (this.exampleSpec.before) {
+      this.exampleSpec.before(this.refs.canvas);
+    }
   }
 
   componentWillUnmount () {
     this.testChangeSource.close();
+    if (this.examples.after) {
+      this.exampleSpec.after(this.refs.canvas);
+    }
   }
 
   render () {
     return (
       <div className='example'>
-        <div className='example-canvas' dangerouslySetInnerHTML={this.renderExample()} />
+        <div className='example-canvas' ref="canvas" dangerouslySetInnerHTML={this.renderExample()} />
         <iframe
           ref='iframe'
           className='test-suite'
