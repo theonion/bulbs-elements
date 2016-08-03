18ac8d1b582181c6718260fc18b8eae9472f5d01
Chris Sprehe <chris.sprehe@gmail.com>
2016-08-02 21:16:40 -0500

    Baseline of the bulbs-datamap object

---
+++ elements/bulbs-datamap/bulbs-datamap-examples.js
@@ -0,0 +1,15 @@
+let examples = {
+  element: 'bulbs-datamap',
+  examples: {
+    'Basic Example': {
+      render: function () {
+        return `
+          <bulbs-datamap>
+          </bulbs-datamap>
+        `;
+      },
+    },
+  },
+};
+
+export default examples;
