b9c661174ace710102e1eb2d5d31fe74ffd2b40e
Collin Miller (Zardulist) <collintmiller@gmail.com>
2016-08-22 14:01:50 -0500

    update cinemagraph

--- elements/bulbs-cinemagraph/bulbs-cinemagraph-examples.js
+++ elements/bulbs-cinemagraph/bulbs-cinemagraph-examples.js
@@ -1,6 +1,23 @@
 export default {
   element: 'bulbs-cinemagraph',
   examples: {
+    'out of view': {
+      render () {
+        return `
+          <div
+            style="
+              height: 2000px;
+            "
+          >
+          </div>
+          <video
+            is="bulbs-cinemagraph"
+            cinemagraph-duration="1.55"
+            src="//v.theonion.com/onionstudios/video/4307/640.mp4"
+          />
+        `;
+      },
+    },
     '[src] attribute': {
       render () {
         return `
