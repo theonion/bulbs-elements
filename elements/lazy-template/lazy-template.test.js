2a380611fbed0161b17ec9b63063c58a36f7d3a4
Collin Miller (Zardulist) <collintmiller@gmail.com>
2016-09-30 12:45:37 -0500

    fuller tests around lazy-template

--- elements/lazy-template/lazy-template.test.js
+++ elements/lazy-template/lazy-template.test.js
@@ -1,40 +1,59 @@
 import './lazy-template';
 
-describe('<script is="lazy-template">', () => {
+describe.only('<script is="lazy-template">', () => {
   let sandbox;
+  let subject;
+  let container;
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
+    subject = makeTemplate(`
+      <h1>Cool Template</h1>
+      <p>It is lazy!</p>
+    `);
+    container = document.createElement('div');
+    document.body.appendChild(container);
   });
 
   afterEach(() => {
     sandbox.restore();
+    container.remove();
   });
 
   function makeTemplate (innerHTML) {
     let template = document.createElement('script', 'lazy-template');
-    template.setAttribute('type', 'tex/html');
+    template.setAttribute('type', 'text/html');
+    template.setAttribute('load-on', 'page-load');
     template.innerHTML = innerHTML;
     return template;
   }
 
-  xit('requires a load-on attribute', () => {
-
+  it('requires a load-on attribute', () => {
+    subject.removeAttribute('load-on');
+    expect(() => {
+      subject.attachedCallback();
+    }).to.throw('<script is="lazy-template"> MUST specify a "load-on" attribute (either "page-load" or "in-view").');
   });
 
-  xit('requires a type set to "text/html"', () => {
+  it('requires a type set to "text/html"', () => {
+    subject.setAttribute('type', 'text/whatever');
+    expect(() => {
+      subject.attachedCallback();
+    }).to.throw('<script is="lazy-template"> MUST set the attribute type="text/html".');
+  });
 
+  it('requires a type attribute', () => {
+    subject.removeAttribute('type');
+    expect(() => {
+      subject.attachedCallback();
+    }).to.throw('<script is="lazy-template"> MUST set the attribute type="text/html".');
   });
 
   describe('replaceWithContents', () => {
     it('replaces itself with the content html', () => {
-      let container = document.createElement('div');
-      let template = makeTemplate(`
-        <h1>Cool Template</h1>
-        <p>It is lazy!</p>
-      `);
-      container.appendChild(template);
-      template.replaceWithContents();
+      container = document.createElement('div');
+      container.appendChild(subject);
+      subject.replaceWithContents();
 
       expect(container.children[0].outerHTML).to.eql('<h1>Cool Template</h1>');
       expect(container.children[1].outerHTML).to.eql('<p>It is lazy!</p>');
@@ -42,18 +61,37 @@ describe('<script is="lazy-template">', () => {
   });
 
   context('load-on="page-load"', () => {
-    xit('sets content when page load event fires', () => {
+    beforeEach(() => subject.setAttribute('load-on', 'page-load'));
 
+    xit('sets content when page load event fires', () => {
+      // document.readyState is  'complete' by the time this test starts
+      // and it can't be overwritten, not sure how to test this
     });
 
-    xit('sets content immediately if page load event has fired', () => {
+    it('sets content immediately if page load event has fired', (done) => {
+      container.appendChild(subject);
 
+      setImmediate(() => {
+        expect(container.children[0].outerHTML).to.eql('<h1>Cool Template</h1>');
+        expect(container.children[1].outerHTML).to.eql('<p>It is lazy!</p>');
+        done();
+      });
     });
   });
 
   context('load-on="in-view"', () => {
-    xit('sets content when enterviewport event fires', () => {
+    beforeEach(() => subject.setAttribute('load-on', 'in-view'));
 
+    it('sets content when enterviewport event fires', (done) => {
+      container.appendChild(subject);
+
+      setImmediate(() => {
+        expect(container.children[0].tagName).to.eql('SCRIPT');
+        subject.dispatchEvent(new CustomEvent('enterviewport'));
+        expect(container.children[0].outerHTML).to.eql('<h1>Cool Template</h1>');
+        expect(container.children[1].outerHTML).to.eql('<p>It is lazy!</p>');
+        done();
+      });
     });
   });
 });
