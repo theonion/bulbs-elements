aa95c496a873d8abaf756c045f96bf9eefdcd854
Jamie Levinson <jlevinson@theonion.com>
2016-10-31 14:09:27 -0500

    Adding a check for ad block

--- elements/bulbs-video/elements/rail-player/rail-player.js
+++ elements/bulbs-video/elements/rail-player/rail-player.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { registerReactElement } from 'bulbs-elements/register';
 import BulbsElement from 'bulbs-elements/bulbs-element';
+import detectAdBlock from 'bulbs-elements/util/detect-ad-block';
 
 import VideoField from '../../fields/video';
 import VideoRequest from '../../fields/video-request';
@@ -10,6 +11,11 @@ import RailPlayerRoot from './components/root';
 import './rail-player.scss';
 
 export default class RailPlayer extends BulbsElement {
+
+  componentDidMount() {
+    detectAdBlock();
+  }
+
   initialDispatch () {
     this.store.actions.fetchVideo(this.props.src);
   }
