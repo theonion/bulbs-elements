013e07b49d037bd879f2f828de33b5c4efe93b65
Jamie Levinson <jlevinson@theonion.com>
2016-11-10 17:02:34 -0600

    build for tag: temp-6b19109-bulbs-responsive-nav

---
+++ dist/lazy-iframe.js
@@ -0,0 +1,2 @@
+webpackJsonp([29],[function(e,t,r){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}var a=Object.assign||function(e){for(var t=1;t<arguments.length;t++){var r=arguments[t];for(var n in r)Object.prototype.hasOwnProperty.call(r,n)&&(e[n]=r[n])}return e},i=r(175),o=n(i),c=r(2),l=r(181);(0,c.registerReactElement)("lazy-iframe",(0,l.loadOnDemand)(function(e){return o.default.createElement("iframe",a({},e,{style:{width:"100%",height:"100%"}}))}))}]);
+//# sourceMappingURL=lazy-iframe.js.map
