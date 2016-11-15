3db47c619cfe7446aacaeb47748a92df8843e996
Melissa <melpatenator@gmail.com>
2016-11-15 15:32:40 -0600

    wip

--- elements/bulbs-pinned-element/bulbs-pinned-element.js
+++ elements/bulbs-pinned-element/bulbs-pinned-element.js
@@ -1,7 +1,7 @@
 import { registerElement, BulbsHTMLElement } from 'bulbs-elements/register';
 import invariant from 'invariant';
 import { camelCase, isNumber, includes } from 'lodash';
-import { isNumericString } from 'bulbs-elements/util';
+import { isNumericString, getScrollOffset } from 'bulbs-elements/util';
 import '../../node_modules/waypoints/lib/noframework.waypoints';
 import './bulbs-pinned-element.scss';
 
@@ -16,6 +16,7 @@ export default class BulbsPinnedElement extends BulbsHTMLElement {
     invariant(this.hasAttribute('pinned-to'), '<bulbs-pinned-element pinned-to=".selector">: a pinned-to selector is required');
     const selector = this.getAttribute('pinned-to');
     const element = document.querySelector(selector);
+    this.lastPosition = 0;
 
     invariant(element, `<bulbs-pinned-element pinned-to=".selector">: no element with the selector "${selector}" is in the DOM`);
 
@@ -23,6 +24,7 @@ export default class BulbsPinnedElement extends BulbsHTMLElement {
 
     const pinnedClass = this.hasAttribute('pinned-class') ? this.getAttribute('pinned-class') : 'pinned';
     this.outOfViewWaypoint = this.createOutOfViewWaypoint(element, offset, pinnedClass);
+    window.addEventListener('scroll', this.handleScrollEvent.bind(this));
   }
 
   createOutOfViewWaypoint (element, offset, pinnedClass) {
@@ -50,12 +52,77 @@ export default class BulbsPinnedElement extends BulbsHTMLElement {
     return 0;
   }
 
+  handleScrollEvent () {
+    const offset = getScrollOffset();
+    const elementPinnedTo = this.parentElement.querySelector(
+      this.getAttribute('pinned-to')
+    );
+    if (offset.y > this.lastPosition) {
+      this.handleScrollDown(elementPinnedTo);
+    } else {
+      this.handleScrollUp(elementPinnedTo);
+    }
+    this.lastPosition = offset.y;
+  }
+
   elementOutOfViewTop (element) {
     return element.getBoundingClientRect().height * -1;
   }
 
   elementOutOfViewBottom (element) {
     element.getBoundingClientRect().height;
+  }
+
+  handleScrollDown (elementPinnedTo) {
+    if (this.pinnedParentTopInViewport(elementPinnedTo)) {
+      // pin this to top of parent
+      this.pinToParentTop();
+    } else if (this.pinnedElementAtParentBottom(elementPinnedTo)) {
+      // is pinned element at the bottom of the parent
+      this.pinToParentBottom();
+    } else {
+      // pin that shit to the top of screen
+      this.removePinnedClass();
+    }
+  }
+
+  handleScrollUp (elementPinnedTo) {
+    // is this  element at the top of elementPinnedTo?
+    // y? pin to parent top
+    // n? is it at the bottom of element?
+    // y? do nothing
+    // is it in between? fix to top
+  }
+
+  pinnedParentTopInViewport (el) {
+    if(!el) { return; } // delete me
+    let elementTop = $(el).offset().top;
+    let windowTop = window.pageYOffset;
+    let windowBottom = windowTop + window.innerHeight;
+    console.log(elementTop > windowTop && elementTop < windowBottom);
+    return elementTop > windowTop && elementTop < windowBottom;
+  }
+
+  pinnedElementAtParentBottom (el) {
+    if(!el) { return; } // delete me
+    let elBottom = el.getBoundingClientRect().bottom;
+    let pinnedBottom = this.getBoundingClientRect().bottom;
+    return pinnedBottom >= elBottom;
+  }
+
+  pinToParentTop() {
+    this.classList.remove('pinned-bottom');
+    this.classList.add('pinned-top');
+  }
+
+  pinToParentBottom() {
+    this.classList.remove('pinned-top');
+    this.classList.add('pinned-bottom');
+  }
+
+  removePinnedClass() {
+    this.classList.remove('pinned-top');
+    this.classList.remove('pinned-bottom');
   }
 
   handleOutOfView (element, pinnedClass) {
