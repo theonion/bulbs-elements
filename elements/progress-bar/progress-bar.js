239ece82505e9ed2bc75a5818692cd42c5ac1b3c
Collin Miller (Zardulist) <collintmiller@gmail.com>
2017-01-11 16:30:03 -0600

    watch attribute on progress bar

--- elements/progress-bar/progress-bar.js
+++ elements/progress-bar/progress-bar.js
@@ -6,6 +6,10 @@ function invertProgress (progress) {
 }
 
 class ProgressBar extends BulbsHTMLElement {
+
+  static get observedAttributes() {
+    return ['progress'];
+  }
   connectedCallback () {
     let progress = parseInt(this.getAttribute('progress'), 10) || 0;
     this.innerHTML = `<div class="progress-track" style="width: ${invertProgress(progress)}%"></div>`;
