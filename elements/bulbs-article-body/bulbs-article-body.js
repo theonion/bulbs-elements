d8813cbc7425e25be0d8b71c7886cc9ac5c74d46
Andrew Kos <akos123@gmail.com>
2017-02-09 11:30:19 -0600

    add tests for dingbat

--- elements/bulbs-article-body/bulbs-article-body.js
+++ elements/bulbs-article-body/bulbs-article-body.js
@@ -10,27 +10,27 @@ class BulbsArticleBody extends BulbsHTMLElement {
     let dingbat = '<span class="site-dingbat"></span>';
 
     if (!$lastElement.text()) {
-      $lastElement.prev();
+      $lastElement = $lastElement.prev();
     }
 
     if ($lastElement.is('ul')) {
-      // appends to .article-text > ul > li:last-child
+      // appends to this > ul > li:last-child
       $lastElement.children('li:last-child').append(dingbat);
     } else if ($lastElement.is('div')) {
       if ($lastElement.find('> :last-child').is('p')) {
-        // appends to .article-text > div > p:last-child
+        // appends to this > div > p:last-child
         $lastElement.find('> :last-child').append(dingbat);
       } else {
-        // appends to .article-text > div:last-child
+        // appends to this > div:last-child
         $lastElement.append(dingbat);
       }
     } else if ($lastElement.is('p')) {
       // Handle extraneous br tags
       if($lastElement.children('br').length) {
-        // targets .article-text > p:last-child > before br:last-child
+        // targets this > p:last-child > before br:last-child
         $lastElement.children('br:last-child').before(dingbat);
       } else {
-        // targets .article-text > p:last-child
+        // targets this > p:last-child
         $lastElement.append(dingbat);
       }
     }
