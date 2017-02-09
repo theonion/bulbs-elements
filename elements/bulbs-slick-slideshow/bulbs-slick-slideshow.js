602e15239676ed1fcd6ce4946c06fa4badaa674e
Bryce Dorn <brycedorn@gmail.com>
2017-02-09 13:25:04 -0600

    fix selector, isolate key events, add tests
    lint

--- elements/bulbs-slick-slideshow/bulbs-slick-slideshow.js
+++ elements/bulbs-slick-slideshow/bulbs-slick-slideshow.js
@@ -3,7 +3,8 @@ import {
   BulbsHTMLElement,
 } from 'bulbs-elements/register';
 import $ from 'jquery';
-import * as slick from 'slick-carousel'; // eslint-disable-line
+import * as slick from 'slick-carousel';
+import { InViewMonitor } from 'bulbs-elements/util';
 
 import './bulbs-slick-slideshow.scss';
 
@@ -18,6 +19,10 @@ import './bulbs-slick-slideshow.scss';
 class BulbsSlickSlideshow extends BulbsHTMLElement {
   attachedCallback () {
     this.slideshow = $('.slider');
+
+    if ($('.slider').length > 1) {
+      this.slideshow = $(this).find('.slider');
+    }
 
     // set variables
     this.initialSlide = 0;
@@ -87,11 +92,16 @@ class BulbsSlickSlideshow extends BulbsHTMLElement {
     this.slideshow.slick('slickGoTo', 0);
   }
 
+  isInViewport () {
+    return InViewMonitor.isElementInViewport(this.slideshow[0]);
+  }
+
   bodyKeyDown (event) {
-    if(event.keyCode === 37) { // left
+    const isVisible = this.isInViewport();
+    if(event.keyCode === 37 && isVisible) { // left
       this.slideshow.slick('slickPrev');
     }
-    else if(event.keyCode === 39) { // right
+    else if(event.keyCode === 39 && isVisible) { // right
       this.slideshow.slick('slickNext');
     }
   }
