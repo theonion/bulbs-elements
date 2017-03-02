c768bebcbbc42e9a6cc4c51b53fc96d44b946b6a
collin <collintmiller@gmail.com>
2017-03-02 16:20:44 -0600

    git-cher-index
    Something like this...

--- elements/bulbs-reading-list/components/bulbs-reading-list-item.js
+++ elements/bulbs-reading-list/components/bulbs-reading-list-item.js
@@ -10,6 +10,10 @@ class BulbsReadingListItem extends BulbsHTMLElement {
     invariant(this.dataset.href, '<bulbs-reading-list-menu> requires a "data-href" attribute');
     invariant(this.dataset.partialUrl, '<bulbs-reading-list-menu> requires a "data-partial-url" attribute');
     invariant(this.dataset.title, '<bulbs-reading-list-menu> requires a "data-title" attribute');
+
+    const siblings = this.closest('bulbs-reading-list').querySelectorAll('bulbs-reading-list-item');
+    const index = [].indexOf.call(siblings, this) + 1; // 0 indexed
+    this.setAttribute('data-track-action', `Article: ${index}`);
   }
 }
 
