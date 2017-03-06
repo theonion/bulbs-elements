d13207a9c5420ac42c74fb67fa900e5878c0675e
Collin Miller (Zardulist) <collintmiller@gmail.com>
2017-03-06 12:11:31 -0600

    click to toggle/scroll to close

--- elements/bulbs-nav/bulbs-nav.js
+++ elements/bulbs-nav/bulbs-nav.js
@@ -1,6 +1,8 @@
 import { registerElement, BulbsHTMLElement } from 'bulbs-elements/register';
 import './bulbs-nav.scss';
 
+const CLICK_TOGGLE = location.search === '?nav-toggle=click';
+
 const NavStateManager = {
   state: {},
 
@@ -25,7 +27,12 @@ const NavStateManager = {
 
 class BulbsNavToggle extends BulbsHTMLElement {
   createdCallback () {
-    this.addEventListener('mouseenter', () => this.openNavPanel());
+    if (CLICK_TOGGLE) {
+      this.addEventListener('click', () => this.toggleNavPanel());
+    }
+    else {
+      this.addEventListener('mouseenter', () => this.openNavPanel());
+    }
   }
 
   get navPanel () {
@@ -37,13 +44,23 @@ class BulbsNavToggle extends BulbsHTMLElement {
     NavStateManager.cancelClose(this.navPanel);
     this.navPanel.open();
   }
+
+  toggleNavPanel () {
+    this.navPanel.toggle();
+  }
 }
 
 class BulbsNavPanel extends BulbsHTMLElement {
   createdCallback () {
-    this.addEventListener('mouseleave', () => {
-      NavStateManager.requestClose(this);
-    });
+    if (CLICK_TOGGLE) {
+      // no-op
+    }
+    else {
+      this.addEventListener('mouseleave', () => {
+        NavStateManager.requestClose(this);
+      });
+    }
+    this.scrollHandler = this.scrollHandler.bind(this);
   }
 
   get tabGroup () {
@@ -62,6 +79,12 @@ class BulbsNavPanel extends BulbsHTMLElement {
     );
   }
 
+  scrollHandler () {
+    if (this.getBoundingClientRect().top < -100) {
+      this.close();
+    }
+  }
+
   open () {
     if (!this.classList.contains('bulbs-nav-panel-active')) {
       [].forEach.call(this.otherPanels, otherPanel => otherPanel.close());
@@ -73,12 +96,23 @@ class BulbsNavPanel extends BulbsHTMLElement {
       if (window.picturefill) {
         window.picturefill();
       }
+      window.addEventListener('scroll', this.scrollHandler);
     }
   }
 
   close () {
     this.classList.remove('bulbs-nav-panel-active');
     this.navToggle.classList.remove('bulbs-nav-toggle-active');
+    window.removeEventListener('scroll', this.scrollHandler);
+  }
+
+  toggle () {
+    if (this.classList.contains('bulbs-nav-panel-active')) {
+      this.close();
+    }
+    else {
+      this.open();
+    }
   }
 }
 
