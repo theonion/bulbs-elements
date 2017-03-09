d1d0a38c7c9c56bc714aa67f3e56e60f18887165
Melissa <melpatenator@gmail.com>
2017-03-09 17:16:55 -0600

    blow away `bulbs-reading-list-menu`

--- elements/bulbs-reading-list/bulbs-reading-list.js
+++ elements/bulbs-reading-list/bulbs-reading-list.js
@@ -3,7 +3,6 @@ import './components/bulbs-reading-list-item';
 import './components/bulbs-reading-list-articles';
 import './components/bulbs-reading-list-menu';
 import './components/bulbs-reading-list-menu-item-clickable-area';
-import ReadingListMenu from './lib/reading-list-menu';
 import ReadingListArticles from './lib/reading-list-articles';
 import invariant from 'invariant';
 import EventEmitter from 'events';
@@ -23,9 +22,7 @@ class BulbsReadingList extends BulbsHTMLElement {
     window.addEventListener('resize', this.handleDocumentResized.bind(this));
     this.dispatcher.on('reading-list-item-url-changed', this.handleDocumentUrlChanged.bind(this));
 
-    const menus = this.querySelectorAll(`bulbs-reading-list-menu`);
     const articles = this.querySelectorAll(`bulbs-reading-list-articles`);
-    this.readingListMenus = this.createReadingListMenus(menus);
     this.readingListArticles = this.createReadingListArticles(articles);
   }
 
@@ -61,15 +58,6 @@ class BulbsReadingList extends BulbsHTMLElement {
     this.dispatcher.emit('window-resize', getWindowDimensions());
   }
 
-  createReadingListMenus (menuElements) {
-    invariant(menuElements, 'BulbsReadingList.createReadingListMenus(menuElements): menuElements is undefined');
-    return map(menuElements, this.createReadingListMenu.bind(this));
-  }
-
-  createReadingListMenu (menuElement) {
-    invariant(menuElement, 'BulbsReadingList.createReadingListMenu(menuElement): menuElement is undefined');
-    return new ReadingListMenu(menuElement, this.dispatcher);
-  }
 
   createReadingListArticles (articlesElements) {
     invariant(articlesElements, 'BulbsReadingList.createReadingListArticles(articlesElements): articlesElements is undefined');
