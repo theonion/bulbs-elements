3b44ebd4998b720aaacc5b9fe7c65eacd80341db
Andrew Kos <akos123@gmail.com>
2017-04-03 11:26:36 -0500

    fix more bulbs-page issues (#291)
    * also account for hash in bulbs-page scrolling
    
    * fix bulbs-page tests
    
    * new bulbs-page in view logic to take into account mutliple pages being in view
    
    * some linting
    
    * bulbs page fix test
    
    * fix linting issues

--- elements/bulbs-page/bulbs-page.test.js
+++ elements/bulbs-page/bulbs-page.test.js
@@ -47,11 +47,11 @@ describe('<bulbs-page>', () => {
       expect(InViewMonitor.add).to.have.been.calledWith(element).once;
     });
 
-    it('adds listener for pagestart', () => {
+    it('adds listener for inviewrect', () => {
       element.attachedCallback();
-      sandbox.spy(element, 'handlePageStart');
-      element.dispatchEvent(new CustomEvent('pagestart'));
-      expect(element.handlePageStart).to.have.been.called.once;
+      sandbox.stub(element, 'handleInView');
+      element.dispatchEvent(new CustomEvent('inviewrect'));
+      expect(element.handleInView).to.have.been.called.once;
     });
 
     it('adds a document ready handler', () => {
@@ -77,6 +77,22 @@ describe('<bulbs-page>', () => {
         expect(LockScroll.lockToElement).to.have.been.calledWith(element).once;
       });
 
+      it('locks to element if is current page and hash matches', () => {
+        sinon.stub(element, 'isCurrentPage').returns(true);
+
+        element.handleDocumentReady();
+
+        expect(LockScroll.lockToElement).to.have.been.calledWith(element).once;
+      });
+
+      it('is no-op if current page but hash does not match', () => {
+        sinon.stub(element, 'isCurrentPage').returns(false);
+
+        element.handleDocumentReady();
+
+        expect(LockScroll.lockToElement).to.not.have.been.called;
+      });
+
       it('is no-op if not current page', () => {
         expect(LockScroll.lockToElement).to.not.have.been.called;
       });
@@ -89,10 +105,63 @@ describe('<bulbs-page>', () => {
     });
   });
 
-  describe('handlePageStart', () => {
+  describe('getElementRatioOfWindow', () => {
+    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+
+    it('calculates ratio of window element takes up when element is over the top', () => {
+      const rectBot = windowHeight - 10;
+      element.cachedViewRect = {
+        bottom: rectBot,
+        top: -10,
+      };
+
+      const ratio = element.getElementRatioOfWindow();
+
+      expect(ratio).to.equal(rectBot / windowHeight);
+    });
+
+    it('calculates ratio of window element takes up when element is under the bottom', () => {
+      const rectTop = 10;
+      element.cachedViewRect = {
+        bottom: windowHeight + 10,
+        top: rectTop,
+      };
+
+      const ratio = element.getElementRatioOfWindow();
+
+      expect(ratio).to.equal((windowHeight - rectTop) / windowHeight);
+    });
+
+    it('calculates ratio of window element takes up when element is entirely in the window', () => {
+      const rectHeight = windowHeight - 10;
+      element.cachedViewRect = {
+        bottom: windowHeight - 5,
+        height: rectHeight,
+        top: 5,
+      };
+
+      const ratio = element.getElementRatioOfWindow();
+
+      expect(ratio).to.equal(rectHeight / windowHeight);
+    });
+
+    it('calculates ratio of window element takes up when element takes up the entire window or more', () => {
+      element.cachedViewRect = {
+        bottom: windowHeight + 5,
+        top: -5,
+      };
+
+      const ratio = element.getElementRatioOfWindow();
+
+      expect(ratio).to.be.above(1);
+    });
+  });
+
+  describe('handleInViewAndInFocus', () => {
+
     it('calls replaceState api', () => {
       mockRaf.cancel();
-      element.handlePageStart();
+      element.handleInViewAndInFocus();
       mockRaf.step();
       expect(history.replaceState).to.have.been.calledWith(
         {},
@@ -102,13 +171,49 @@ describe('<bulbs-page>', () => {
     });
 
     it('tracks a pageview', () => {
-      element.handlePageStart();
+      element.handleInViewAndInFocus();
       requestAnimationFrame(() => {
         expect(util.getAnalyticsManager().trackPageView).to.have.been.calledWith(
           '/example',
           'Pushstate Title',
         ).once;
       });
+    });
+  });
+
+  describe('handleInView', () => {
+
+    it('when multiple pages are in view, it only performs actions for the one with majority screen realestate', () => {
+      const page2 = createElement('bulbs-page', {
+        data: {
+          contentAnalyticsDimensions: { dimension1: 'one' },
+        },
+      });
+      const ratioInView = 0.6;
+      page2.setAttribute('pushstate-title', 'Second Page');
+      page2.setAttribute('pushstate-url', '/example-2');
+      sinon.stub(page2, 'handleInViewAndInFocus');
+      sinon.stub(page2, 'getElementRatioOfWindow').returns(ratioInView);
+      sinon.stub(element, 'handleInViewAndInFocus');
+      sinon.stub(element, 'getElementRatioOfWindow').returns(1 - ratioInView);
+      mockRaf.cancel();
+
+      element.handleInView({
+        detail: {
+          boundingRect: {},
+        },
+      });
+      page2.handleInView({
+        detail: {
+          boundingRect: {},
+        },
+      });
+      mockRaf.step();
+
+      expect(element.handleInViewAndInFocus).to.not.have.been.called;
+      expect(element.getElementRatioOfWindow).to.have.been.called;
+      expect(page2.handleInViewAndInFocus).to.have.been.called;
+      expect(page2.getElementRatioOfWindow).to.have.been.called;
     });
   });
 });
