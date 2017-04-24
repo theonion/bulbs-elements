f36abfe4caa213cb86f9872b0b54f3fb2d71d17a
Jamie Levinson <jlevinson@theonion.com>
2017-04-24 15:17:26 -0500

    Update invariant

--- elements/bulbs-pdfobject/bulbs-pdfobject-bulbs-cms.js
+++ elements/bulbs-pdfobject/bulbs-pdfobject-bulbs-cms.js
@@ -9,7 +9,8 @@ import PDFObject from 'pdfobject/pdfobject';
 
 class BulbsPDFObject extends BulbsHTMLElement {
   createdCallback () {
-    invariant(this.hasAttribute, 'src');
+    invariant(this.hasAttribute('poster'), '<bulbs-pdfobject> MUST have a \'poster\' attribute');
+    invariant(this.hasAttribute('src'), '<bulbs-pdfobject> MUST have a \'src\' attribute');
     PDFObject.embed(this.getAttribute('src'), this);
   }
 }
