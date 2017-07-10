629e4e608da25a4cc1aeb9eb98934ad29efbe893
Chris Sprehe <chris.sprehe@gmail.com>
2017-07-10 16:40:08 -0400

    build for tag: temp-0255c80-third-party-build

---
+++ dist/lazy-iframe.js
@@ -0,0 +1,2 @@
+webpackJsonp([41],[function(e,t,r){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}var a=Object.assign||function(e){for(var t=1;t<arguments.length;t++){var r=arguments[t];for(var n in r)Object.prototype.hasOwnProperty.call(r,n)&&(e[n]=r[n])}return e},i=r(24),o=n(i),c=r(127),l=r(6);(0,c.registerReactElement)("lazy-iframe",(0,l.loadOnDemand)(function(e){return o.default.createElement("iframe",a({},e,{style:{width:"100%",height:"100%"}}))}))}]);
+//# sourceMappingURL=lazy-iframe.js.map
