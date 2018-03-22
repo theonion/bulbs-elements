8f0bf4b3fcf7ccee1a9e328e00ac3f8ecea0ad6a
Chris Sprehe <chris.sprehe@gmail.com>
2018-03-22 14:23:20 -0500

    Added ability to configure split pic through attributes

--- elements/bulbs-splitpic/bulbs-splitpic.js
+++ elements/bulbs-splitpic/bulbs-splitpic.js
@@ -8,6 +8,22 @@ class BulbsSplitpic extends BulbsHTMLElement {
     invariant(this.attributes.orientation.value, 'BulbsSplitpic: orientation attribute is required');
     this.orientation = this.attributes.orientation.value;
 
+    if (this.attributes['bs-right-image']) {
+      this.setImageSrc(this.attributes['bs-right-image'].value, 'right');
+    }
+
+    if (this.attributes['bs-left-image']) {
+      this.setImageSrc(this.attributes['bs-left-image'].value, 'left');
+    }
+
+    if (this.attributes['bs-image-size']) {
+      this.setImageSize(this.attributes['bs-image-size'].value);
+    }
+
+    if (this.attributes['bs-start-percent']) {
+      this.setStartPercent(this.attributes['bs-start-percent'].value);
+    }
+
     if (this.orientation === 'horizontal') {
       this.splitPic = new SplitPic(this);
     }
@@ -15,6 +31,27 @@ class BulbsSplitpic extends BulbsHTMLElement {
     if (this.orientation === 'vertical') {
       this.splitPic = new SplitPicVertical(this);
     }
+  }
+
+  setImageSize (size) {
+    const $element = $(this);
+
+    let $imageContainer = $element.find('.splitpic-images');
+    $imageContainer.addClass(`size-${size}`);
+  }
+
+  setStartPercent (percent) {
+    const $element = $(this);
+    let $imageContainer = $element.find('.splitpic-images');
+
+    $imageContainer.data('start-percent', percent);
+  }
+
+  setImageSrc (imageUrl, direction) {
+    let $element = $(this);
+    let imgNode = $element.find(`.splitpic-${direction}-image img`)[0];
+
+    imgNode.src = imageUrl;
   }
 }
 
